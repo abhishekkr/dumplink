94f19be8fb96385638bd67c19aa01f04ae98401f
AbhishekKr <abhikumar163@gmail.com>
2013-11-12 10:46:30 +0530

    dumplink server improved, /links added

--- dumplink-server/dumplinks.go
+++ dumplink-server/dumplinks.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "fmt"
+  "flag"
   "net/http"
   "html/template"
   "encoding/json"
@@ -10,37 +11,54 @@ import (
   "io/ioutil"
 )
 
+type Link map[string]string
 type Links struct {
-  Urls []string
+  Urls []Link
 }
 
-var linkfile = "./public/static/links.json"
+var (
+  httpuri   = flag.String("uri", "127.0.0.1", "what PORT to run the DumpLink daemon")
+  httpport  = flag.String("port", "8100", "what PORT to run the DumpLink daemon")
+  linkfile  = flag.String("json", "./public/static/links.json", "path to json file to save links, need {'url':[]}")
+)
 
-func not_in_list(new_item string, list []string) bool{
+func not_in_list(new_link string, list []Link) bool{
   for _, item := range list {
-    if item == new_item {
-      return false
+    for link, _ := range item {
+      if link == new_link { return false }
     }
   }
   return true
 }
 
-func save_json(new_url string) bool{
-  contents,_ := ioutil.ReadFile(linkfile)
+func read_json() string{
+  contents, err := ioutil.ReadFile(*linkfile)
+  if err != nil {
+    fmt.Println("Error:", err)
+    return "Error: JSON gave error while reading."
+  }
+  return string(contents)
+}
+
+func save_json(new_url string, title string) bool{
+  contents,_ := ioutil.ReadFile(*linkfile)
   var dump Links
   err := json.Unmarshal([]byte(contents), &dump)
   if err != nil {
     fmt.Println("Error:", err)
     return false
   }
+  var _link Link
+  _link = make(Link)
+  _link[new_url] = title
 
   if not_in_list(new_url, dump.Urls) {
-    dump.Urls = append(dump.Urls, new_url)
+    dump.Urls = append(dump.Urls, _link)
   } else {
     return true
   }
   b, _ := json.Marshal(&dump)
-  ioutil.WriteFile(linkfile, b, 0x644)
+  ioutil.WriteFile(*linkfile, b, 0x644)
   return true
 }
 
@@ -48,7 +66,7 @@ func DumpLink(w http.ResponseWriter, req *http.Request) {
   w.Header().Set("Content-Type", "text/html")
 
   t, _ := template.ParseFiles("public/bookmarklet.html")
-  t.Execute(w, nil)
+  t.Execute(w, map[string]string {"HTTPURI": *httpuri, "HTTPPort": *httpport})
 }
 
 func Dump(w http.ResponseWriter, req *http.Request) {
@@ -56,7 +74,7 @@ func Dump(w http.ResponseWriter, req *http.Request) {
   req.ParseForm()
   fmt.Println(req.FormValue("url"))
   fmt.Println(req.FormValue("title"))
-  save_json(req.FormValue("url"))
+  save_json(req.FormValue("url"), req.FormValue("title"))
 
   t, _ := template.ParseFiles("public/status.html")
   t.Execute(w, nil)
@@ -66,13 +84,13 @@ func GetLinks(w http.ResponseWriter, req *http.Request) {
   w.Header().Set("Content-Type", "text/html")
 
   t, _ := template.ParseFiles("public/status.html")
-  t.Execute(w, nil)
+  t.Execute(w, map[string]string {"BODY": read_json()})
 }
 
 
 func main(){
   runtime.GOMAXPROCS(runtime.NumCPU())
-  httpport := 8000
+  flag.Parse()
 
   http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./public/static/"))))
   http.HandleFunc("/", DumpLink)
@@ -82,11 +100,12 @@ func main(){
   http.HandleFunc("/links", GetLinks)
 
   srv := &http.Server{
-    Addr:        fmt.Sprintf(":%d", httpport),
+    Addr:        fmt.Sprintf("%s:%s", *httpuri, *httpport),
     Handler:     http.DefaultServeMux,
     ReadTimeout: time.Duration(5) * time.Second,
   }
 
-  fmt.Printf("access your goshare at http://<IP>:%d\n", httpport)
-  srv.ListenAndServe()
+  fmt.Printf("access your goshare at http://%s:%s\n", *httpuri, *httpport)
+  err := srv.ListenAndServe()
+  fmt.Println("Game Over:", err)
 }
